ed5c332c2474ba690e6a40678277c4e3821ddc20
Guido Ferrari <guido_ferrari@live.com>
2020-10-24 19:17:23 -0300

    still dont know what im doing

--- scala/src/main/scala/tadp/parser/Parser.scala
+++ scala/src/main/scala/tadp/parser/Parser.scala
@@ -2,12 +2,18 @@ package tadp.parser
 
 import scala.util.Try
 
-trait Parser {
-  def run(string: String): Try[Object]
+trait Parser[T] {
+  def run(string: String): Try[T]
 }
 
-class anyChar{
+class anyChar extends Parser[Char]{
   def run(string: String): Try[Char] = {
     Try(string.charAt(0))
+  }
+}
+
+class char(char: Char) extends Parser[Char]{
+  def run(string: String): Try[Char] = {
+    Try(if (string.charAt(0) != char) {throw new Error}).map{boolean => char}
   }
 }
--- scala/src/test/scala/tadp/parser/ParserSpec.scala
+++ scala/src/test/scala/tadp/parser/ParserSpec.scala
@@ -5,13 +5,29 @@ import org.scalatest.matchers._
 
 class ParserSpec extends AnyFlatSpec with should.Matchers {
   it should "Parsearme exitosamente un string" in {
-    val resultado = new anyChar().run("Hola")
+    val resultado = new anyChar().run("hola")
     assert(resultado.isSuccess)
-    assert(resultado.get == 'H')
+    assert(resultado.get == 'h')
   }
 
   it should "Devolverme failure si le mando un string vacio" in {
     val resultado = new anyChar().run("")
+    assert(resultado.isFailure)
+  }
+
+  it should "Devolverme éxito si parseo un string que tiene el caracter deseado" in {
+    val resultado = new char('c').run("coca")
+    assert(resultado.isSuccess)
+    assert(resultado.get == 'c')
+  }
+
+  it should "Devolverme failure si parseo un string que no tiene el caracter deseado" in {
+    val resultado = new char('c').run("papa")
+    assert(resultado.isFailure)
+  }
+
+  it should "Devolverme failure si parseo un string vacio" in {
+    val resultado = new char('c').run("")
     assert(resultado.isFailure)
   }
 }

04c1182813ebcdfd62e73734eafa4fa02fd928a8
Guido Ferrari <guido_ferrari@live.com>
2020-10-25 23:05:45 -0300

    Agrego parsers

--- scala/src/main/scala/tadp/parser/Parser.scala
+++ scala/src/main/scala/tadp/parser/Parser.scala
@@ -7,13 +7,37 @@ trait Parser[T] {
 }
 
 class anyChar extends Parser[Char]{
-  def run(string: String): Try[Char] = {
+  override def run(string: String): Try[Char] = {
     Try(string.charAt(0))
   }
 }
 
 class char(char: Char) extends Parser[Char]{
-  def run(string: String): Try[Char] = {
+  override def run(string: String): Try[Char] = {
     Try(if (string.charAt(0) != char) {throw new Error}).map{boolean => char}
   }
+}
+
+class digit extends Parser[Char] {
+  override def run(string: String): Try[Char] = {
+    Try(if (!string.charAt(0).isDigit) {
+      throw new Error
+    }).map { boolean => string.charAt(0) }
+  }
+}
+
+class string(stringEsperado: String) extends Parser[String]{
+  override def run(string: String): Try[String] = {
+    Try(
+      if (!string.startsWith(stringEsperado)){throw new Error}
+    ).map{boolean => stringEsperado}
+  }
+}
+
+class integer extends Parser[Integer] {
+  override def run(string: String): Try[Integer] = {Try(string.toInt)}
+}
+
+class double extends Parser[Double] {
+  override def run(string: String): Try[Double] = {Try(string.toDouble)}
 }
--- scala/src/test/scala/tadp/parser/ParserSpec.scala
+++ scala/src/test/scala/tadp/parser/ParserSpec.scala
@@ -30,4 +30,54 @@ class ParserSpec extends AnyFlatSpec with should.Matchers {
     val resultado = new char('c').run("")
     assert(resultado.isFailure)
   }
+
+  it should "Devolverme success si parseo un string que comienza con un digito" in {
+    val resultado = new digit().run("1hola")
+    assert(resultado.isSuccess)
+    assert(resultado.get == '1')
+  }
+
+  it should "Devolverme failure si parseo un string que no comienza con un digito" in {
+    val resultado = new digit().run("hola")
+    assert(resultado.isFailure)
+  }
+
+  it should "Devolverme success si parseo un string que comienza con el string esperado" in {
+    val resultado = new string("hola").run("hola mundo")
+    assert(resultado.isSuccess)
+    assert(resultado.get == "hola")
+  }
+
+  it should "Devolverme failure si parseo un string que no comienza con el string esperado" in {
+    val resultado = new string("hola").run("hol1")
+    assert(resultado.isFailure)
+  }
+
+  it should "Devolverme success si parseo un string que es un digito positivo" in {
+    val resultado = new integer().run("1234")
+    assert(resultado.isSuccess)
+    assert(resultado.get == 1234)
+  }
+
+  it should "Devolverme success si parseo un string que es un digito negativo" in {
+    val resultado = new integer().run("-1234")
+    assert(resultado.isSuccess)
+    assert(resultado.get == -1234)
+  }
+
+  it should "Devolverme failure si parseo un string que no es un digito" in {
+    val resultado = new integer().run("hola")
+    assert(resultado.isFailure)
+  }
+
+  it should "Devolverme success si parseo un string que es un double" in {
+    val resultado = new double().run("1234.14")
+    assert(resultado.isSuccess)
+    assert(resultado.get == 1234.14)
+  }
+
+  it should "Devolverme failure si parseo un string que no es un double" in {
+    val resultado = new double().run("hola")
+    assert(resultado.isFailure)
+  }
 }

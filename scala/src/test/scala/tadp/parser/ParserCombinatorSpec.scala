9bb9d5e111997e67d32307392194222a59468b55
Guido Ferrari <guido_ferrari@live.com>
2020-11-07 09:41:12 -0300

    Lo hago con try

--- scala/src/test/scala/tadp/parser/ParserCombinatorSpec.scala
+++ scala/src/test/scala/tadp/parser/ParserCombinatorSpec.scala
@@ -1,183 +1,124 @@
 package tadp.parser
-
 import org.scalatest.flatspec._
 import org.scalatest.matchers._
+import tadp.parser.Parser.{char, digit, integer, string}
 
 class ParserCombinatorSpec extends AnyFlatSpec with should.Matchers {
 
   it should "Combino dos parser con el <|> y debe devolver success" in {
-    val parser1 = new char('c')
-    val parser2 = new char('h')
-    val parsersCombinados = parser1 <|> parser2
-
-    val resultado = parsersCombinados("hola")
-
-    assert(resultado == ResultadoExitoso('h', "ola"))
+    val parsersCombinados = char('c') <|> char('h')
+    assert(parsersCombinados("hola").get == ('h', "ola"))
   }
 
   it should "Combino dos parser con el <|> con distintos tipos y debe devolver success" in {
-    val parser1 = new char('h')
-    val parser2 = new integer()
-    val parsersCombinados = parser1 <|> parser2
-
-    val resultado = parsersCombinados("10")
-
-    assert(resultado == ResultadoExitoso(10, ""))
+    val parsersCombinados = char('h') <|> integer()
+    assert(parsersCombinados("10").get == (10, ""))
   }
 
   it should "Combino dos parser con el <|> y debe devolver failure" in {
-    val parser1 = new char('c')
-    val parser2 = new char('h')
-    val parsersCombinados = parser1 <|> parser2
-
-    val resultado = parsersCombinados("adios")
-
-    assert(resultado == ResultadoFallido("adios"))
+    val parsersCombinados = char('c') <|> char('h')
+    assert(parsersCombinados("adios").isFailure)
   }
 
   it should "Combino dos parser con el <|> y debe devolver failure por string vacío" in {
-    val parser1 = new char('c')
-    val parser2 = new char('h')
-    val parsersCombinados = parser1 <|> parser2
-
-    val resultado = parsersCombinados("")
-
-    assert(resultado == ResultadoFallido(""))
+    val parsersCombinados = char('c') <|> char('h')
+    assert(parsersCombinados("").isFailure)
   }
 
   it should "Combino dos parser con el <> y debe devolver success" in {
-    val parser1 = new string("hola")
-    val parser2 = new string("mundo")
-    val parsersCombinados = parser1 <> parser2
-
-    val resultado = parsersCombinados("holamundo")
-
-    assert(resultado == ResultadoExitoso(("hola", "mundo"), ""))
+    val parsersCombinados = string("hola") <> string("mundo")
+    assert(parsersCombinados("holamundo").get == (("hola", "mundo"), ""))
   }
 
   it should "Combino dos parser con el <> y distintos tipos y debe devolver success" in {
-    val parser1 = new char('c')
-    val parser2 = new integer()
-    val parsersCombinados = parser1 <> parser2
-
-    val resultado = parsersCombinados("c123")
-
-    assert(resultado == ResultadoExitoso(('c', 123), ""))
+    val parsersCombinados = char('c') <> integer()
+    assert(parsersCombinados("c123").get == (('c', 123), ""))
   }
 
   it should "Combino dos parser con el <> y debe devolver failure" in {
-    val parser1 = new string("hola")
-    val parser2 = new string("mundo")
-    val parsersCombinados = parser1 <> parser2
-
-    val resultado = parsersCombinados("holachau")
-
-    assert(resultado == ResultadoFallido("holachau"))
+    val parsersCombinados = string("hola") <> string("mundo")
+    assert(parsersCombinados("holachau").isFailure)
   }
 
   it should "Combino dos parser con el <> y debe devolver failure por string vacío" in {
-    val parser1 = new string("hola")
-    val parser2 = new string("mundo")
-    val parsersCombinados = parser1 <> parser2
-
-    val resultado = parsersCombinados("")
-
-    assert(resultado == ResultadoFallido(""))
+    val parsersCombinados = string("hola") <> string("mundo")
+    assert(parsersCombinados("").isFailure)
   }
 
   it should "Combino dos parser con el ~> y debe devolver success" in {
-    val parser1 = new string("hola")
-    val parser2 = new string("mundo")
-    val parsersCombinados = parser1 ~> parser2
-
-    val resultado = parsersCombinados("holamundo")
-
-    assert(resultado == ResultadoExitoso("mundo", ""))
+    val parsersCombinados = string("hola") ~> string("mundo")
+    assert(parsersCombinados("holamundo").get == ("mundo", ""))
   }
 
   it should "Combino dos parser con el ~> con dos tipos y debe devolver success" in {
-    val parser1 = new string("hola")
-    val parser2 = new integer()
-    val parsersCombinados = parser1 ~> parser2
-
-    val resultado = parsersCombinados("hola1234")
-
-    assert(resultado == ResultadoExitoso(1234, ""))
+    val parsersCombinados = string("hola") ~> integer()
+    assert(parsersCombinados("hola1234").get == (1234, ""))
   }
 
   it should "Combino dos parser con el ~> y debe devolver failure" in {
-    val parser1 = new string("hola")
-    val parser2 = new string("mundo")
-    val parsersCombinados = parser1 ~> parser2
-
-    val resultado = parsersCombinados("holachau")
-
-    assert(resultado == ResultadoFallido("holachau"))
+    val parsersCombinados = string("hola") ~> string("mundo")
+    assert(parsersCombinados("holachau").isFailure)
   }
 
   it should "Combino dos parser con el ~> y debe devolver failure por string vacío" in {
-    val parser1 = new string("hola")
-    val parser2 = new string("mundo")
-    val parsersCombinados = parser1 ~> parser2
-
-    val resultado = parsersCombinados("")
-
-    assert(resultado == ResultadoFallido(""))
+    val parsersCombinados = string("hola") ~> string("mundo")
+    assert(parsersCombinados("").isFailure)
   }
 
   it should "Combino dos parser con el <~ y debe devolver success" in {
-    val parser1 = new string("hola")
-    val parser2 = new string("mundo")
-    val parsersCombinados = parser1 <~ parser2
-
-    val resultado = parsersCombinados("holamundo")
-
-    assert(resultado == ResultadoExitoso("hola", ""))
+    val parsersCombinados = string("hola") ~> string("mundo")
+    assert(parsersCombinados("holamundo").get == ("mundo", ""))
   }
 
   it should "Combino dos parser con el <~ con distintos tipos y debe devolver success" in {
-    val parser1 = new string("hola")
-    val parser2 = new integer()
-    val parsersCombinados = parser1 <~ parser2
-
-    val resultado = parsersCombinados("hola1234")
-
-    assert(resultado == ResultadoExitoso("hola", ""))
+    val parsersCombinados = string("hola") <~ integer()
+    assert(parsersCombinados("hola1234").get == ("hola", ""))
   }
 
   it should "Combino dos parser con el <~ y debe devolver failure" in {
-    val parser1 = new string("hola")
-    val parser2 = new string("mundo")
-    val parsersCombinados = parser1 <~ parser2
-
-    val resultado = parsersCombinados("holachau")
-
-    assert(resultado == ResultadoFallido("holachau"))
+    val parsersCombinados = string("hola") <~ string("mundo")
+    assert(parsersCombinados("holachau").isFailure)
   }
 
   it should "Combino dos parser con el <~ y debe devolver failure por string vacío" in {
-    val parser1 = new string("hola")
-    val parser2 = new string("mundo")
-    val parsersCombinados = parser1 <~ parser2
-
-    val resultado = parsersCombinados("")
-
-    assert(resultado == ResultadoFallido(""))
+    val parsersCombinados = string("hola") <~ string("mundo")
+    assert(parsersCombinados("").isFailure)
   }
 
-//    it should "Combino dos parser con el sepBy y debe devolver success" in {
-//      val parsersCombinados = new integer().sepBy(new char('-'))
-//
-//      val resultado = parsersCombinados("4356-1234")
-//
-//      assert(resultado == ResultadoExitoso((4356,1234), ""))
-//    }
-//
-//    it should "Combino dos parser con el sepBy y debe devolver failure" in {
-//      val parsersCombinados = new integer().sepBy(new char('-'))
-//
-//      val resultado = parsersCombinados("4356 1234")
-//
-//      assert(resultado == ResultadoFallido("4356 1234"))
-//    }
+  it should "Combino dos parser con el sepBy con distintos tipos y depende de lo que parsea devuelve exito o fallo" in {
+    val parsersCombinados = integer().sepBy(char('-'))
+    assert(parsersCombinados("4356-1234").get == ((4356, 1234), ""))
+    assert(parsersCombinados("43561234").isFailure)
+    assert(parsersCombinados("-").isFailure)
+  }
+
+  it should "Modifico parser con satisfies y debe comportarse correctamente" in {
+    val parserCombinados = integer().satisfies(_ == 456)
+    assert(parserCombinados("456").get == (456, ""))
+    assert(parserCombinados("457").isFailure)
+  }
+
+  it should "Convierto resultado de parser en opcional" in {
+    val talVezIn = string("in").opt
+    val precedencia = talVezIn <> string("fija")
+    assert(precedencia("infija").get == ((Some("in"), "fija"), ""))
+    assert(precedencia("fija").get == ((None, "fija"), ""))
+  }
+
+  it should "Modifico parser con *" in {
+    assert(digit().*("999").get == (List('9', '9', '9'), ""))
+    assert(digit().*("a").get == (List(), "a"))
+    assert(char('a').*("aaa4").get == (List('a', 'a', 'a'), "4"))
+  }
+
+  it should "Modifico parser con +" in {
+    assert(digit().+("999").get == (List('9', '9', '9'), ""))
+    assert(digit().+("a").isFailure)
+    assert(char('a').+("aaa4").get == (List('a', 'a', 'a'), "4"))
+  }
+
+  it should "Modifico resultado del parser con map" in {
+    assert(string("1234").map(_.toInt)("1234").get == (1234, ""))
+    assert(string("abcd").map(_.toList)("abcd").get == (List('a', 'b', 'c', 'd'), ""))
+  }
 }
--- scala/src/test/scala/tadp/parser/ParserSpec.scala
+++ scala/src/test/scala/tadp/parser/ParserSpec.scala
@@ -1,111 +1,70 @@
 package tadp.parser
-
 import org.scalatest.flatspec._
 import org.scalatest.matchers._
+import tadp.parser.Parser.{anyChar, char, digit, double, integer, string}
 
 class ParserSpec extends AnyFlatSpec with should.Matchers {
-  it should "Parsearme exitosamente un string" in {
-    val parser = new anyChar()
-    val resultado = parser("hola")
-
-    assert(resultado == ResultadoExitoso('h', "ola"))
+  it should "Testeo comportamiento de anyChar" in {
+    assert(anyChar()("hola").get == ('h', "ola"))
   }
 
   it should "Devolverme failure si le mando un string vacio" in {
-    val parser = new anyChar()
-    val resultado = parser("")
-
-    assert(resultado == ResultadoFallido(""))
+    assert(anyChar()("").isFailure)
   }
 
   it should "Devolverme éxito si parseo un string que tiene el caracter deseado" in {
-    val parser = new char('c')
-    val resultado = parser("coca")
-
-    assert(resultado == ResultadoExitoso('c', "oca"))
+    assert(char('c')("coca").get == ('c', "oca"))
   }
 
   it should "Devolverme failure si parseo un string que no tiene el caracter deseado" in {
-    val parser = new char('c')
-    val resultado = parser("papa")
-
-    assert(resultado == ResultadoFallido("papa"))
+    assert(char('c')("papa").isFailure)
   }
 
   it should "Devolverme failure si parseo un string vacio" in {
-    val parser = new char('c')
-    val resultado = parser("")
-
-    assert(resultado == ResultadoFallido(""))
+    assert(char('c')("").isFailure)
   }
 
   it should "Devolverme success si parseo un string que comienza con un digito" in {
-    val parser = new digit()
-    val resultado = parser("1hola")
-
-    assert(resultado == ResultadoExitoso('1', "hola"))
+    assert(digit()("1hola").get == ('1', "hola"))
   }
 
   it should "Devolverme failure si parseo un string que no comienza con un digito" in {
-    val parser = new digit()
-    val resultado = parser("hola")
+    assert(digit()("hola").isFailure)
+  }
 
-    assert(resultado == ResultadoFallido("hola"))
+  it should "Devolverme success si parseo un string que es el string esperado" in {
+    assert(string("hola")("hola").get == ("hola", ""))
   }
 
   it should "Devolverme success si parseo un string que comienza con el string esperado" in {
-    val parser = new string("hola")
-    val resultado = parser("hola mundo")
-
-    assert(resultado == ResultadoExitoso("hola"," mundo"))
+    assert(string("hola")("hola mundo").get == ("hola"," mundo"))
   }
 
   it should "Devolverme failure si parseo un string que no comienza con el string esperado" in {
-    val parser = new string("hola")
-    val resultado = parser("hol1")
-
-    assert(resultado == ResultadoFallido("hol1"))
+    assert(string("hola")("hol1").isFailure)
   }
 
   it should "Devolverme failure si parseo un string que es vacio" in {
-    val parser = new string("hola")
-    val resultado = parser("")
-
-    assert(resultado == ResultadoFallido(""))
+    assert(string("hola")("").isFailure)
   }
 
   it should "Devolverme success si parseo un string que es un digito positivo" in {
-    val parser = new integer()
-    val resultado = parser("1234")
-
-    assert(resultado == ResultadoExitoso(1234, ""))
+    assert(integer()("1234").get == (1234, ""))
   }
 
   it should "Devolverme success si parseo un string que es un digito negativo" in {
-    val parser = new integer()
-    val resultado = parser("-1234")
-
-    assert(resultado == ResultadoExitoso(-1234, ""))
+    assert(integer()("-1234").get == (-1234, ""))
   }
 
   it should "Devolverme failure si parseo un string que no es un digito" in {
-    val parser = new integer()
-    val resultado = parser("hola")
-
-    assert(resultado == ResultadoFallido("hola"))
+    assert(integer()("hola").isFailure)
   }
 
   it should "Devolverme success si parseo un string que es un double" in {
-    val parser = new double()
-    val resultado = parser("1234.14")
-
-    assert(resultado == ResultadoExitoso(1234.14, ""))
+    assert(double()("1234.14").get == (1234.14, ""))
   }
 
   it should "Devolverme failure si parseo un string que no es un double" in {
-    val parser = new double()
-    val resultado = parser("hola")
-
-    assert(resultado == ResultadoFallido("hola"))
+    assert(double()("hola").isFailure)
   }
 }

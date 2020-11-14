dcf5ec8cb469e9e82e29e0f68bfbba648a8751ac
Guido Ferrari <guido_ferrari@live.com>
2020-11-14 14:10:08 -0300

    Agrego color

--- scala/src/main/scala/tadp/PictureParser.scala
+++ scala/src/main/scala/tadp/PictureParser.scala
@@ -1,5 +1,7 @@
 package tadp
 
+import scalafx.scene.paint.Color
+import tadp.PictureParser.PicturePrinter
 import tadp.internal._
 import tadp.parser.Parser._
 
@@ -58,46 +60,50 @@ import scala.util.Try
 package object PictureParser {
   type punto = (Double, Double)
 
-  trait formaGeometrica {
+  trait imprimible {
     def print(adapter: TADPDrawingAdapter): Unit
   }
 
-  case class triangulo(v1: punto, v2: punto, v3: punto) extends formaGeometrica {
+  case class triangulo(v1: punto, v2: punto, v3: punto) extends imprimible {
     def print(adapter: TADPDrawingAdapter): Unit = {
       adapter.triangle(v1, v2, v3)
     }
   }
-  case class rectangulo(v1: punto, v2: punto) extends formaGeometrica {
+
+  case class rectangulo(v1: punto, v2: punto) extends imprimible {
     def print(adapter: TADPDrawingAdapter): Unit = {
       adapter.rectangle(v1, v2)
     }
   }
-  case class circulo(v: punto, radio: Int) extends formaGeometrica {
+
+  case class circulo(v: punto, radio: Int) extends imprimible {
     def print(adapter: TADPDrawingAdapter): Unit = {
       adapter.circle(v, radio)
     }
   }
-  case class grupo(formas: List[formaGeometrica]) extends formaGeometrica {
+
+  case class grupo(formas: List[imprimible]) extends imprimible {
     def print(adapter: TADPDrawingAdapter): Unit = {
       formas.foreach(forma => forma.print(adapter))
     }
   }
 
-  case class PicturePrinter() extends (String => Unit){
-    def apply(formaDescripta: String): Unit = {
-    val imagen = (trianguloParser() <|> rectanguloParser() <|> circuloParser() <|> grupoParser()) (formaDescripta)
-
-    TADPDrawingAdapter.forScreen( adapter => {
-      imagen.get match {
-        case (formaGeometrica, _) => formaGeometrica.print(adapter)
-        case _ => println("El parser falla")
-      }
-    })
+  case class color(r: Int, g: Int, b: Int, formas: List[imprimible]) extends imprimible {
+    def print(adapter: TADPDrawingAdapter): Unit = {
+      adapter.beginColor(Color.rgb(r, g, b))
+      formas.foreach(forma => forma.print(adapter))
+      //adapter.end()
     }
   }
 
-  case class trianguloParser() extends Parser[formaGeometrica] {
-    override def apply(input: String): Try[ParserResult[formaGeometrica]] = for {
+  case class parserGenerico() extends Parser[imprimible] {
+    override def apply(input: String): Try[ParserResult[imprimible]] = {
+      (trianguloParser() <|> rectanguloParser() <|> circuloParser() <|> grupoParser() <|> colorParser()) (input)
+    }
+  }
+
+  case class trianguloParser() extends Parser[imprimible] {
+    override def apply(input: String): Try[ParserResult[imprimible]] = for {
       (_, resto) <- string("triangulo[")  (input)
       ((x1, y1), resto) <- (integer().sepBy(string(" @ ")) <~ string(", ")) (resto)
       ((x2, y2), resto) <- (integer().sepBy(string(" @ ")) <~ string(", ")) (resto)
@@ -106,8 +112,8 @@ package object PictureParser {
     } yield (triangulo((x1, y1), (x2, y2), (x3, y3)), resto)
   }
 
-  case class rectanguloParser() extends Parser[formaGeometrica] {
-    override def apply(input: String): Try[ParserResult[formaGeometrica]] = for {
+  case class rectanguloParser() extends Parser[imprimible] {
+    override def apply(input: String): Try[ParserResult[imprimible]] = for {
       (_, resto) <- string("rectangulo[")  (input)
       ((x1, y1), resto) <- (integer().sepBy(string(" @ ")) <~ string(", ")) (resto)
       ((x2, y2), resto) <- integer().sepBy(string(" @ ")) (resto)
@@ -115,8 +121,8 @@ package object PictureParser {
     } yield (rectangulo((x1, y1),(x2, y2)), resto)
   }
 
-  case class circuloParser() extends Parser[formaGeometrica] {
-    override def apply(input: String): Try[ParserResult[formaGeometrica]] = for {
+  case class circuloParser() extends Parser[imprimible] {
+    override def apply(input: String): Try[ParserResult[imprimible]] = for {
       (_, resto) <- string("circulo[")  (input)
       ((x1, y1), resto) <- (integer().sepBy(string(" @ ")) <~ string(", ")) (resto)
       (r, resto) <- integer()(resto)
@@ -127,11 +133,42 @@ package object PictureParser {
   case class grupoParser() extends Parser[grupo] {
     override def apply(input: String): Try[ParserResult[grupo]] = for {
       (_, resto) <- string("grupo(") (input)
-      (formaGeometrica, resto) <- ((trianguloParser() <|> rectanguloParser() <|> circuloParser() <|> grupoParser()) <~ string(", ")).+ (resto)
-      (formaSinComa, resto) <- (trianguloParser() <|> rectanguloParser() <|> circuloParser() <|> grupoParser()) (resto)
+      (formaGeometrica, resto) <- (parserGenerico() <~ string(", ")).+ (resto)
+      (formaSinComa, resto) <- parserGenerico() (resto)
       (_, resto) <- char(')') (resto)
     } yield (grupo(formaGeometrica.appended(formaSinComa)), resto)
   }
+
+  case class colorParser() extends Parser[color] {
+    override def apply(input: String): Try[ParserResult[color]] = for {
+      (_, resto) <- string("color[") (input)
+      (((r, g), b), resto) <- (((integer() <~ string(", ")) <> (integer() <~ string(", ")) <> integer()) <~ string("](")) (resto)
+      (formaGeometrica, resto) <- (parserGenerico() <~ string(", ")).* (resto)
+      (formaSinComa, resto) <- parserGenerico() (resto)
+      (_, resto) <- char(')') (resto)
+    } yield (color(r, g, b, formaGeometrica.appended(formaSinComa)), resto)
+  }
+
+  case class PicturePrinter() extends (String => Unit){
+    def apply(formaDescripta: String): Unit = {
+      val imagen = parserGenerico() (formaDescripta)
+
+      TADPDrawingAdapter.forScreen( adapter => {
+        imagen.get match {
+          case (formaGeometrica, _) => formaGeometrica.print(adapter)
+          case _ => println("El parser falla")
+        }
+      })
+    }
+  }
 }
 
+object app extends App {
+  // grupo anidado
+  //PicturePrinter()("grupo(grupo(triangulo[250 @ 150, 150 @ 300, 350 @ 300], triangulo[150 @ 300, 50 @ 450, 250 @ 450]), grupo(rectangulo[460 @ 90, 470 @ 100], rectangulo[450 @ 100, 480 @ 260]))")
 
+  // color
+  //PicturePrinter()("color[60, 150, 200](grupo(triangulo[200 @ 50, 101 @ 335, 299 @ 335], circulo[200 @ 350, 100]))")
+
+
+}
